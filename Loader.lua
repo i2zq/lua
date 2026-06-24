68bf4eedfb9eba8dedc8fbc5d4d75923372f0b45
i2zq <alazmyhswnh@gmail.com>
2026-06-24 19:41:06 +0300

    .

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,10 @@
 local placeId = game.PlaceId
 if placeId == 71213902019049 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Cross%20Rivers%20For%20Brainrot.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Cross%20Rivers%20For%20Brainrot.lua"))()
 elseif placeId == 93883679965128 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Cross%20the%20Lava%20for%20Brainrot.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Cross%20the%20Lava%20for%20Brainrot.lua"))()
 elseif placeId == 77691995204147 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Shovel%20Snow%20for%20Brainrots.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Shovel%20Snow%20for%20Brainrots.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Protected_6307158492143768.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/main.lua"))()
 end
