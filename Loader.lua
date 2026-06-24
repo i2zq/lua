5cc5a698d658af472e24b0c0d862bed19f904359
i2zq <alazmyhswnh@gmail.com>
2026-06-24 12:45:44 +0300

    .

--- Loader.lua
+++ Loader.lua
@@ -4,5 +4,5 @@ if placeId == 71213902019049 then
 elseif placeId == 93883679965128 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Cross%20the%20Lava%20for%20Brainrot.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Protected_6307158492143768.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/main.lua"))()
 end
