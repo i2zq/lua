c9431b6afd19cc84332c06e03e9d4b489dde5a96
i2zq <alazmyhswnh@gmail.com>
2026-06-28 22:05:09 +0300

    .

--- Loader.lua
+++ Loader.lua
@@ -5,8 +5,10 @@ elseif placeId == 93883679965128 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Cross%20the%20Lava%20for%20Brainrot.lua"))()
 elseif placeId == 77691995204147 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Shovel%20Snow%20for%20Brainrots.lua"))()
-elseif placeId == 97598239454123 then
+elseif placeId == 10200395747 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Grow%20a%20Garden2.lua"))()
+elseif placeId == 9731911476 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/Games/Size%20Legends%20Simulator.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/i2zq/lua/refs/heads/main/main.lua"))()
 end
